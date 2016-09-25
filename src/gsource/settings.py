5851a8b50dc289f85f3561e19c0c7d8d4ef74ef0
kais <kais0016@yahoo.fr>
2016-09-25 22:23:40 +0200

    Add user & Account app

--- src/gsource/settings.py
+++ src/gsource/settings.py
@@ -46,6 +46,8 @@ INSTALLED_APPS = [
     'conferencier',
     'competition',
     'poste',
+    'account',
+    'User',
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -128,3 +130,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS=(
+os.path.join(BASE_DIR,"static"),
+'/static/',
+)
+MEDIA_ROOT=os.path.join(BASE_DIR,'media')
+MEDIA_URL='/media/'
